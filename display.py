f2c26e49d34b17f0407039cd923565d8dc82bd5b
Stavros Korokithakis <hi@stavros.io>
2024-05-29 23:44:13 +0300

    Add arguments

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import os
 import sys
 import time
@@ -9,58 +10,80 @@ from PIL import Image
 from PIL import ImageDraw, ImageOps, ImageEnhance
 from waveshare_epd import epd7in3f
 
-palette = [
-    0,
-    0,
-    0,  # Color 1: black
-    255,
-    0,
-    0,  # Color 2: red
-    0,
-    255,
-    0,  # Color 3: green
-    0,
-    0,
-    255,  # Color 4: blue
-    255,
-    255,
-    0,  # Color 5: yellow
-    255,
-    128,
-    0,  # Color 6: orange
-    255,
-    255,
-    255,  # Color 7: white
-]
 
-image = Image.open(sys.argv[1])
-image = ImageOps.fit(image, (800, 480))
+def display(path, brightness, method="fit"):
+    palette = [
+        0,
+        0,
+        0,  # Color 1: black
+        255,
+        0,
+        0,  # Color 2: red
+        0,
+        255,
+        0,  # Color 3: green
+        0,
+        0,
+        255,  # Color 4: blue
+        255,
+        255,
+        0,  # Color 5: yellow
+        255,
+        128,
+        0,  # Color 6: orange
+        255,
+        255,
+        255,  # Color 7: white
+    ]
 
-brightness = float(sys.argv[2])
-if brightness > 1:
-    image = ImageEnhance.Contrast(image).enhance(1 / brightness)
-    image = ImageEnhance.Brightness(image).enhance(brightness)
+    image = Image.open(path)
+    if method == "fit":
+        image = ImageOps.fit(image, (800, 480))
+    elif method == "pad":
+        image = ImageOps.pad(image, (800, 480), color=(255, 255, 255))
 
-# Create a new image using the palette
-palette_image = Image.new("P", (1, 1))
-palette_image.putpalette(
-    palette * int(256 / len(palette)) + palette[: 3 * (256 % len(palette))]
-)
+    image = image.convert("RGB")
 
-im = image.quantize(palette=palette_image, dither=Image.Dither.FLOYDSTEINBERG)
-im = im.transpose(Transpose.ROTATE_180)
+    if brightness > 1:
+        image = ImageEnhance.Contrast(image).enhance(1 / brightness)
+        image = ImageEnhance.Brightness(image).enhance(brightness)
 
-try:
-    epd = epd7in3f.EPD()
-    epd.init()
-    epd.Clear()
+    # Create a new image using the palette
+    palette_image = Image.new("P", (1, 1))
+    palette_image.putpalette(
+        palette * int(256 / len(palette)) + palette[: 3 * (256 % len(palette))]
+    )
 
-    epd.display(epd.getbuffer(im))
-    time.sleep(3)
+    im = image.quantize(palette=palette_image, dither=Image.Dither.FLOYDSTEINBERG)
+    im = im.transpose(Transpose.ROTATE_180)
 
-    epd.sleep()
-except IOError as e:
-    print(e)
-except KeyboardInterrupt:
-    epd7in3f.epdconfig.module_exit(cleanup=True)
-    exit()
+    try:
+        epd = epd7in3f.EPD()
+        epd.init()
+        epd.Clear()
+
+        epd.display(epd.getbuffer(im))
+        time.sleep(3)
+
+        epd.sleep()
+    except IOError as e:
+        print(e)
+    except KeyboardInterrupt:
+        epd7in3f.epdconfig.module_exit(cleanup=True)
+        return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process an image file.")
+    parser.add_argument("filename", type=str, help="Path to the image file")
+    parser.add_argument(
+        "--brightness", type=float, default=1, help="Adjust the brightness level"
+    )
+    parser.add_argument(
+        "--method", type=str, default="fit", help="Method for processing the image"
+    )
+
+    args = parser.parse_args()
+
+    print("Showing image...")
+    display(args.filename, args.brightness, args.method)
